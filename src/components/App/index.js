662db1169132c445af74a4fa22ebac11047ca8c1
LorenzovandenDungen <2130766@talnet.nl>
2023-02-28 13:21:21 +0100

    save

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navigation from '../Navigation';
 
 
 class App extends Component {
